1416fab014c16996014336e831c1a57114aad057
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-07-02 17:38:36 +0530

    updated

--- lib/screens/cert.dart
+++ lib/screens/cert.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class Certificate extends StatelessWidget {
   String? name;
+  final day = DateTime.now().day.toString();
+  final month = DateTime.now().month.toString();
+  final year = DateTime.now().year.toString();
   Certificate({Key? key, this.name}) : super(key: key);
 
   @override
@@ -9,6 +12,7 @@ class Certificate extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
               'Hi, $name',
@@ -16,16 +20,11 @@ class Certificate extends StatelessWidget {
                 fontSize: 50,
                 fontWeight: FontWeight.bold,
               ),
-            ),
-            const SizedBox(
-              height: 20,
+              textAlign: TextAlign.start,
             ),
             const Image(
               image: NetworkImage(
                   'https://cdn.discordapp.com/attachments/836229655606067220/992751235415429150/unknown.png'),
-            ),
-            const SizedBox(
-              height: 40,
             ),
             const Text(
               'You have successfully completed Hybrid Mobile App Development Course.',
@@ -33,15 +32,22 @@ class Certificate extends StatelessWidget {
                 fontSize: 25,
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
             const Text(
               'INSTRUCTOR NAME',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                fontSize: 25,
+                fontSize: 20,
               ),
+            ),
+            const Text(
+              'Pankaj Kapoor',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Text('Date: $day/$month/$year'),
             )
           ],
         ),
